7312bdd52bc38610721e8b38982473db49f43e55
mousta7sane <mousta7sane@users.noreply.github.com>
2020-04-06 23:24:02 +0100

    Create the front and back end projects

---
+++ todoapp-back/src/main/java/com/moustahsane/todoappback/TodoappBackApplication.java
@@ -0,0 +1,13 @@
+package com.moustahsane.todoappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappBackApplication.class, args);
+	}
+
+}
---
+++ todoapp-back/src/test/java/com/moustahsane/todoappback/TodoappBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moustahsane.todoappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
